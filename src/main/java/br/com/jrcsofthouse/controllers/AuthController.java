208568579e036ee94187bd6c77646a2c6bb13c7c
Jorge Coutinho <juniorjrc>
2020-08-26 19:53:15 -0300

    Aula38 - Implementando a controller de autenticação

---
+++ src/main/java/br/com/jrcsofthouse/controllers/AuthController.java
@@ -0,0 +1,67 @@
+package br.com.jrcsofthouse.controllers;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jrcsofthouse.repository.UserRepository;
+import br.com.jrcsofthouse.security.AccountCredentialsVO;
+import br.com.jrcsofthouse.security.jwt.JwtTokenProvider;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	UserRepository respository;
+	
+	@ApiOperation(value="Autenticar usuário por credencial")
+	@PostMapping(value = "/signin", produces = {"application/json", "application/xml", "application/x-yaml"},
+			consumes = {"application/json", "application/xml", "application/x-yaml"})	
+	public ResponseEntity signIn(@RequestBody AccountCredentialsVO data) {
+		try {
+			var username = data.getUserName();
+			var password = data.getPassword();
+			
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+			
+			var user = respository.findByUserName(username);
+			
+			var token = "";
+			
+			if(user != null) {
+				token = tokenProvider.createToken(username, user.getRoles());
+			}else {
+				throw new UsernameNotFoundException("Username " + username + " not found!!!");
+			}
+			
+			Map<Object, Object> model = new HashMap<>();
+			model.put("username", username);
+			model.put("token", token);
+			
+			return ok(model);
+		} catch (AuthenticationException e) {
+			throw new BadCredentialsException("Invalid username/password supplied");
+		} 
+	}
+}
