5fcab9c78fa873c7d055a5125705f5101132d10c
Jorge Coutinho <juniorjrc>
2020-08-28 22:44:04 -0300

    Aula47 - Iniciando implementações de upload de arquivo

--- src/main/java/br/com/jrcsofthouse/Main.java
+++ src/main/java/br/com/jrcsofthouse/Main.java
@@ -3,10 +3,16 @@ package br.com.jrcsofthouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.com.jrcsofthouse.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class Main {
@@ -14,10 +20,12 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 		
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
-		String result = bCryptPasswordEncoder.encode("admin123");
-		System.out.println("===========================================");
-		System.out.println("Senha do usuário administrador = " + result);
-		System.out.println("===========================================");
+		/*
+		 * BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(16);
+		 * String result = bCryptPasswordEncoder.encode("admin123");
+		 * System.out.println("===========================================");
+		 * System.out.println("Senha do usuário administrador = " + result);
+		 * System.out.println("===========================================");
+		 */
 	}
 }
