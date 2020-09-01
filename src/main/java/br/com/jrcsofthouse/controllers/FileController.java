2416ed26419d20be6f5dcd0ea08e02b90dc81582
Jorge Coutinho <juniorjrc>
2020-08-31 23:15:52 -0300

    Aula53 - Implementando integração com o DOCKER da aplicação spring

--- src/main/java/br/com/jrcsofthouse/controllers/FileController.java
+++ src/main/java/br/com/jrcsofthouse/controllers/FileController.java
@@ -4,7 +4,17 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +30,8 @@ import io.swagger.annotations.Api;
 @RestController
 @RequestMapping("api/file/v1")
 public class FileController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 	
 	@Autowired
 	private FileStorageService fileStorageService;
@@ -42,6 +54,27 @@ public class FileController {
 				.stream()
 				.map(file -> uploadFile(file))
 				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/downloadFile/{fileName:.+}")
+	public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+		Resource resource = fileStorageService.loadFileAsResource(fileName);
+		
+		String contentType = null;
+		
+		try {
+			contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+		} catch (Exception e) {
+			logger.info("Could not determinate file type");
+		}
+		
+		if(contentType == null) {
+			contentType = "application/octet-stream";
+		}
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(contentType))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+				.body(resource);
 	}
 
 }
